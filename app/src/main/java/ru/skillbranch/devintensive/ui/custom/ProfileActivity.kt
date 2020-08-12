05e8bd39635c0ffad7e45adb6d38b6d8a477281a
fooxmobile <fooxer_skill@mail.ru>
2020-08-12 23:51:53 +0300

    learn how to constraint

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package ru.skillbranch.devintensive.ui.custom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.skillbranch.devintensive.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
